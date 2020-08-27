201a0bafac3b0d910c7d44117d34da482f174659
Mateusz Jakowlew <mtjako@Mateuszs-Mac-mini.local>
2020-08-27 22:30:06 +0200

    BugFix

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home(props) {
   const [weather, setWeather] = useState({"coord":{"lon":15.51,"lat":51.94},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":280.62,"feels_like":28.85,"temp_min":26,"temp_max":30,"pressure":1012,"humidity":41},"visibility":10000,"wind":{"speed":1.5,"deg":0},"clouds":{"all":82},"dt":1597922403,"sys":{"type":1,"id":1714,"country":"PL","sunrise":1597895458,"sunset":1597947117},"timezone":7200,"id":3080165,"name":"Zielona Góra","cod":200});
 
   useEffect(() => {
-      fetch('http://api.openweathermap.org/data/2.5/weather?id=3080165&appid=e186820a755e6badc03e15fe1819552c&units=metric').then(res => {
+      fetch('https://api.openweathermap.org/data/2.5/weather?id=3080165&appid=e186820a755e6badc03e15fe1819552c&units=metric').then(res => {
           return res.json();
       }).then(function(res) {
           setWeather(res);
